af1b7a3f5edee12985b1c2a9c32f419a1e806f82
Elian Doran <elinnum@gmail.com>
2019-02-08 07:00:15 +0200

    Draw original image on a canvas

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function getRandomImageURL() {
 // Load image
 var sourceImage = document.getElementById("source-image");
 sourceImage.src = getRandomImageURL();
+sourceImage.onload = function() {
+    drawImage(this);
+};
+
+// Prepare canvas
+function drawImage(image) {
+    var destCanvas = document.getElementById("destination-canvas");
+    destCanvas.width = image.width;
+    destCanvas.height = image.height;
+
+    var ctx = destCanvas.getContext("2d");
+    ctx.drawImage(image, 0, 0);
+}
