96d1ed36fc46a4b599719db879f62ebf0d7e65f4
Elian Doran <elinnum@gmail.com>
2019-02-08 07:41:33 +0200

    Draw image scaled

--- index.js
+++ index.js
@@ -6,14 +6,32 @@ function getRandomImageURL() {
 var sourceImage = document.getElementById("source-image");
 sourceImage.src = getRandomImageURL();
 sourceImage.onload = function() {
-    drawImage(this);
+    drawImage(this, {
+        message: "Hello world.",
+        width: 800,
+        height: 600
+    });
+};
+
+// Form
+var form = document.getElementById("entry-form");
+form.onsubmit = function() {
+    const data = new FormData(form);
+    console.log(data);
+
+    return false;
 };
 
 // Prepare canvas
-function drawImage(image) {
+function drawImage(image, data) {
+    var message = (data.message);
+    console.log(data);
+
     var destCanvas = document.getElementById("destination-canvas");
-    destCanvas.width = image.width;
-    destCanvas.height = image.height;
+    destCanvas.width = data.width;
+    destCanvas.height = data.height;
+    destCanvas.style.width = image.width + "px";
+    destCanvas.style.height = image.height + "px";
 
     var ctx = destCanvas.getContext("2d");
     ctx.drawImage(image, 0, 0);
